58924ef8851526d8d92cf716378d12225f1b2b8c
AneriP <aneripothiwala91@github.com>
2020-12-23 21:54:01 -0800

    1

--- js/index.js
+++ js/index.js
@@ -4,23 +4,41 @@ console.log("Your index.js file is loaded correc")
 
 
 $('.circle').on("click", function(){
-    
+    $(".listup").removeClass("listup");
+    $(".textup").removeClass("textup");
     $(this).children().toggleClass("textup");
     $(this).children('ul').toggleClass("listup");
+    // $(".footernav").css("margin-bottom", "300px");
+    
+});
 
 
-      });
+$('.circle1').on("click", function(){
+$(".footernav").css("margin-bottom", "200px");
+});
+
+$('.circle2').on("click", function(){
+    $(".footernav").css("margin-bottom", "300px");
+    });
 
 
 
 
 
-      $(document).ready(function() {
+$(".leftcontent h2").animate({ right:"-200px" , opacity: "1"  }, 2000)
 
-        $("#imagelogo").click(function(){
-           $(".target").effect( "bounce", {times:1}, 1000 );
-        });
 
-     });
+
+
+
+
+
+    //   $(document).ready(function() {
+
+    //     $("#imagelogo").click(function(){
+    //        $(".target").effect( "bounce", {times:1}, 1000 );
+    //     });
+
+    //  });
 
 
